ef58a5f835f6fb1c7e34c4f9eb22df57e659d1b9
Chandru <157028392+chandru2110@users.noreply.github.com>
2026-01-30 11:03:09 +0530

    Add files via upload

---
+++ primenumbercheck.java
@@ -0,0 +1,12 @@
+package project1.pst;
+import java.util.Scanner;
+public class primenumbercheck {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		int num=scanner.nextInt();
+		
+
+	}
+
+}
