1f1c2cabab20256065b50b27e5537e6d880b8832
Chandru <157028392+chandru2110@users.noreply.github.com>
2026-01-30 12:16:27 +0530

    Add files via upload

--- primenumbercheck.java
+++ primenumbercheck.java
@@ -1,12 +1,29 @@
 package project1.pst;
 import java.util.Scanner;
+
 public class primenumbercheck {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		Scanner scanner=new Scanner(System.in);
-		int num=scanner.nextInt();
-		
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
 
-	}
+        int temp = 0;
 
+        if (num <= 1) {
+            temp = 1;   
+        } else {
+            for (int i = 2; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    temp = 1;
+                    break;
+                }
+            }
+        }
+
+        if (temp == 0)
+            System.out.println(num + " is a Prime number");
+        else
+            System.out.println(num + " is NOT a Prime number");
+    }
 }
