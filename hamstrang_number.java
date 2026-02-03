851deb77381b9c294afcb12242424e16a80290a7
Chandru <157028392+chandru2110@users.noreply.github.com>
2026-02-03 10:13:54 +0530

    Add files via upload

---
+++ hamstrang_number.java
@@ -0,0 +1,19 @@
+package project1.pst;
+import java.util.Scanner;
+public class hamstrang_number {
+
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int num=sc.nextInt();
+	int count=0;
+	int rem=1;
+	while(num!=0) {
+		num=num/10;
+		count++;
+	}
+		
+	
+
+	}
+
+}
