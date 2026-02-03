205d8bed893fa0040d2961c6e448bb457505010d
Chandru <157028392+chandru2110@users.noreply.github.com>
2026-02-03 13:42:11 +0530

    Add files via upload

---
+++ oops.java
@@ -0,0 +1,51 @@
+package project1.pst;
+
+public class oops {
+
+    public static void main(String[] args) {
+    	
+        heisenberg();  
+        walterwhite();
+        
+        Department dep=new Department();
+        dep.Cse_3_year();
+       dep.cse_4_year();
+    }
+
+    public static void jessepinkman() {
+        System.out.println("he is good boy");
+        
+    }
+    public static void walterwhite() {
+    	System.out.println("chemistary teacher");
+    	
+    }
+    public static void heisenberg() {
+    
+    	System.out.println("the cook");
+    }
+    public static void mike() {
+    	System.out.println("chill guy");
+    }
+    public static void gus() {
+    	System.out.println("the cartal");
+    }
+}
+ class Department{
+	public static void main(String[]arg) {
+		Department dep=new Department();
+		Cse_3_year();
+		}
+	public static void Cse_1_year() {
+		System.out.println("cse first year");
+	}
+	public static void Cse_2_year() {
+		System.out.println("cse second year");
+	}
+	public static void Cse_3_year() {
+		System.out.println("cse pre-final year ");
+		}
+	public static void cse_4_year(){
+		System.out.println("cse final year");
+	}
+ }
